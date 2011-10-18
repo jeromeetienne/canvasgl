e9bd66f2c37e96ef784daf3c4e1241918ccf7958
Jerome Etienne <jerome.etienne@gmail.com>
2011-10-18 22:04:11 +0200

    more work

--- learningwebgl99/index.js
+++ learningwebgl99/index.js
@@ -1,55 +1,55 @@
-    var gl;
-
-    function initGL(canvas) {
-        try {
-            gl = canvas.getContext("experimental-webgl");
-            gl.viewportWidth = canvas.width;
-            gl.viewportHeight = canvas.height;
-        } catch (e) {
-        }
-        if (!gl) {
-            alert("Could not initialise WebGL, sorry :-(");
-        }
-    }
-
-
-    function getShader(gl, id) {
-        var shaderScript = document.getElementById(id);
-        if (!shaderScript) {
-            return null;
-        }
-
-        var str = "";
-        var k = shaderScript.firstChild;
-        while (k) {
-            if (k.nodeType == 3) {
-                str += k.textContent;
-            }
-            k = k.nextSibling;
-        }
-
-        var shader;
-        if (shaderScript.type == "x-shader/x-fragment") {
-            shader = gl.createShader(gl.FRAGMENT_SHADER);
-        } else if (shaderScript.type == "x-shader/x-vertex") {
-            shader = gl.createShader(gl.VERTEX_SHADER);
-        } else {
-            return null;
-        }
-
-        gl.shaderSource(shader, str);
-        gl.compileShader(shader);
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert(gl.getShaderInfoLog(shader));
-            return null;
-        }
-
-        return shader;
-    }
-
-
-    var shaderProgram;
+	var gl;
+	
+	function initGL(canvas) {
+	    try {
+		gl = canvas.getContext("experimental-webgl");
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	    } catch (e) {
+	    }
+	    if (!gl) {
+		alert("Could not initialise WebGL, sorry :-(");
+	    }
+	}
+	
+	
+	function getShader(gl, id) {
+	    var shaderScript = document.getElementById(id);
+	    if (!shaderScript) {
+		return null;
+	    }
+	
+	    var str = "";
+	    var k = shaderScript.firstChild;
+	    while (k) {
+		if (k.nodeType == 3) {
+		    str += k.textContent;
+		}
+		k = k.nextSibling;
+	    }
+	
+	    var shader;
+	    if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	    } else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	    } else {
+		return null;
+	    }
+	
+	    gl.shaderSource(shader, str);
+	    gl.compileShader(shader);
+	
+	    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(shader));
+		return null;
+	    }
+	
+	    return shader;
+	}
+	
+	
+	var shaderProgram;
 
 	function initShaders() {
 		var fragmentShader = getShader(gl, "shader-fs");
@@ -133,47 +133,57 @@
     var cubeVertexTextureCoordBuffer;
     var cubeVertexIndexBuffer;
 
-    function initBuffers() {
-        cubeVertexPositionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-        vertices = [
-// TODO this should be 2D
+function initBuffers() {
+	var viewportW	= 512;
+	var viewportH	= 512;
+	
+	var pixelXToPosition	= function(x){
+		return (x - viewportW/2) / (viewportW/2);
+	}
+	var pixelYToPosition	= function(y){
+		return (x - viewportH/2) / (viewportH/2);
+	}
+	var buildPosition	= function(x,y, width, height){
+		var minX	= pixelXToPosition(x);
+		var maxX	= pixelXToPosition(x+width-1);
+		var minY	= pixelYToPosition(y);
+		var maxY	= pixelYToPosition(y+height-1);
+	}
+	
+	cubeVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+	vertices = [
+	// TODO this should be 2D
 		-1.0, -1.0,  0.0,
 		 1.0, -1.0,  0.0,
 		 1.0,  1.0,  0.0,
 		-1.0,  1.0,  0.0,
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        cubeVertexPositionBuffer.itemSize = 3;
-        cubeVertexPositionBuffer.numItems = 4;
-
-        cubeVertexTextureCoordBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-        var textureCoords = [
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	cubeVertexPositionBuffer.itemSize = 3;
+	cubeVertexPositionBuffer.numItems = 4;
+	
+	cubeVertexTextureCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+	var textureCoords = [
 		0.0, 0.0,
 		1.0, 0.0,
 		1.0, 1.0,
 		0.0, 1.0,
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-        cubeVertexTextureCoordBuffer.itemSize = 2;
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+	cubeVertexTextureCoordBuffer.itemSize = 2;
 	cubeVertexTextureCoordBuffer.numItems = 4;
-
-
-        cubeVertexIndexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-        var cubeVertexIndices = [
-            0, 1, 2,      0, 2, 3,    // Front face
-        ];
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-        cubeVertexIndexBuffer.itemSize = 1;
+	
+	cubeVertexIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+	var cubeVertexIndices = [
+		0, 1, 2,      0, 2, 3,    // Front face
+	];
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+	cubeVertexIndexBuffer.itemSize = 1;
 	cubeVertexIndexBuffer.numItems = 6;
-    }
-
-
-    var xRot = 0;
-    var yRot = 0;
-    var zRot = 0;
+};
 
     function drawScene() {
         gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
